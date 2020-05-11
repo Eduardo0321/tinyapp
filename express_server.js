8fca81f8aa1bfc6bd7442f8b1d091b2ede90b1b4
Eduardo <Eduardo0321@gmail.com>
2020-05-11 20:27:49 +0000

    Simple push

--- express_server.js
+++ express_server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //This is the view engine call. It tells Express app to use EJS.
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls", (req, res) => {           // NEW ROUTER HANDLER TO PASS THE URL DATA TO THE TEMPLATE.
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
