1d24b0cd3ffa4200f78d051463d5d3b8f1e4d236
Eduardo <Eduardo0321@gmail.com>
2020-05-12 05:36:01 +0000

    SAVE

--- express_server.js
+++ express_server.js
@@ -15,7 +15,7 @@ app.get("/urls", (req, res) => {           // NEW ROUTER HANDLER TO PASS THE URL
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase};
+  let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 })
 
