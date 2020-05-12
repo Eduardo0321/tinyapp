487b4be65684cedac83808a5c68ebbfe1d8ebed3
Eduardo <Eduardo0321@gmail.com>
2020-05-12 12:48:07 +0000

    New random number function.

--- express_server.js
+++ express_server.js
@@ -2,12 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs"); //This is the view engine call. It tells Express app to use EJS.
 
 const urlDatabase = {
   "b2xVn2": "http://www.bleacherreport.com",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let url = "";
+  const length = 6;
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++)
+    url += chars.charAt(Math.floor(Math.random() * chars.length));
+  return url
+}
 
 app.get("/urls", (req, res) => {           // NEW ROUTER HANDLER TO PASS THE URL DATA TO THE TEMPLATE.
   let templateVars = { urls: urlDatabase };
@@ -22,6 +34,11 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 })
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
