9175348376f683a8febb85b7e01419b113bcccb3
Eduardo <Eduardo0321@gmail.com>
2020-05-20 00:03:32 +0000

    Git commit test.

--- helpers.js
+++ helpers.js
@@ -1,12 +1,18 @@
 const getUserByEmail = function(email, users) {
   for (let userID in users) {
+    // console.log(userID);
+    // console.log(email);
+    // console.log(users[userID].email);
+    // console.log("---")
     if (users[userID].email === email) {
+      console.log(users[userID]);
       return users[userID];
     } else {
       return undefined;
     }
   }
 };
+
 
 
 module.exports = { getUserByEmail };
